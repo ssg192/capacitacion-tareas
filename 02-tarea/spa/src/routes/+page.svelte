64a5fefeb4039b23ab9a49c9250df5f3a620c584
Salvador Sandoval <developer5.vwd@gmail.com>
2026-07-28 10:11:44 -0600

    feat: first commit

---
+++ 02-tarea/spa/src/routes/+page.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  let modoOscuro = $state(false);
+  
+  function cambiarModo() {
+    if (modoOscuro === true) {
+      modoOscuro = false;
+    } else {
+      modoOscuro = true;
+    }
+  }
+  
+</script>
+
+<div class="contenedor" class:oscuro={modoOscuro}>
+  {#if modoOscuro === true}
+    <button class="boton-oscuro" onclick={cambiarModo}>Cambiar a modo claro</button>
+  {:else}
+    <button class="boton-claro" onclick={cambiarModo}>Cambiar a modo oscuro</button>
+  {/if}
+</div>
+
+<style>
+  .contenedor {
+    background-color: white;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: background-color 1.5s ease;
+  }
+
+  .contenedor.oscuro {
+    background-color: black;
+  }
+
+  .boton-oscuro {
+    padding: 10px 20px;
+    font-size: 16px;
+    border: 1px solid white;
+    border-radius: 6px;
+    background-color: white;
+  }
+
+  .boton-claro {
+    padding: 10px 20px;
+    font-size: 16px;
+    border: 1px solid #333;
+    border-radius: 6px;
+    background-color: white;
+  }
+</style>
