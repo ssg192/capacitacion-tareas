486dab7b25c65e71be8a1207f618cf659e4c81a0
Salvador Sandoval <developer5.vwd@gmail.com>
2026-07-28 13:22:33 -0600

    feat: first commit

--- 02-tarea/ssr/ssr/src/lib/services/get-users.ts
+++ 02-tarea/ssr/ssr/src/lib/services/get-users.ts
@@ -3,10 +3,13 @@ import type { Users } from '$lib/services/Users';
 const API_URL = 'https://rickandmortyapi.com/api';
 
 export async function getCharacters(): Promise<Users[]> {
-		const response = await fetch(`${API_URL}/character`);
-		if (!response.ok) {
-			throw new Error('CHARACTERS_NOT_FOUND');
-		}
-		const data = await response.json();
-	return data.results;
+	const response = await fetch(`${API_URL}/character`);
+	if (!response.ok) {
+		console.error('Status:', response.status, response.statusText);
+		const body = await response.text();
+		console.error('Body:', body);
+		throw new Error('CHARACTERS_NOT_FOUND');
 	}
+	const data = await response.json();
+	return data.results;
+}
